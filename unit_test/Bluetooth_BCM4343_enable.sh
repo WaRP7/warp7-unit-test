a77bc5d1a0b7531cf7eedeca93c30c7eb0964b8b
Andres Lemos <andres.lemos@nxp.com>
2016-07-29 09:11:54 -0300

    WaRP7 Unit Test Add BT enable.
    Add Bluetooth enablement script.
    
    The script perfomrs the following tasks:
    -Load the firmware
    -Start the BT device
    -Show BT feature summary
    
    Signed-off-by: Andres Lemos <andres.lemos@nxp.com>

---
+++ unit_test/Bluetooth_BCM4343_enable.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#WaRP7 Bluetooth Enable Script
+
+#Reset
+echo 145 > /sys/class/gpio/export 
+echo out > /sys/class/gpio/gpio145/direction 
+echo 0 > /sys/class/gpio/gpio145/value 
+echo 1 > /sys/class/gpio/gpio145/value
+
+#Load Firmware
+hciattach ttymxc2 bcm43xx 3000000 flow -t 20
+
+#Start Bluetooth device
+hci0 up
+hci0 piscan
+hciconfig -a
+hcitool dev
+
